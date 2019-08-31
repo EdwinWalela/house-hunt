5c41adaaacff5234c297b13534465a464affdb49
edwin <edwinwalela@gmail.com>
2019-08-31 21:33:16 +0300

    listing container style

--- client/src/pages/admin/Listing.js
+++ client/src/pages/admin/Listing.js
@@ -4,19 +4,34 @@ import ListingDetails from './ListingDetails';
 
 function Listing(props) {
     return (
-        <div>
-            <img src="https://picsum.photos/200" />
-            <h1 style={textStyle}>Newly Built 3 Bedroom Penthouse on Riverside Drive,Nairobi</h1>
+        <div style={containerStyle}>
+            <img style={imageStyle} src="https://picsum.photos/200" />
+            <p style={textStyle}>Newly Built 3 Bedroom Penthouse on Riverside Drive,Nairobi</p>
             <ListingDetails />
         </div>
     )
 }
 
+const containerStyle = {
+    padding:"20px",
+    border:"solid 1px",
+    width:"90%",
+    maxWidth:"400px",
+    margin:"auto"
+}
+
 const imageStyle = {
-    
+    width:"100px",
+    height:"100px"
 }
 
 const textStyle = {
+    fontSize:"0.8em",
+    paddingLeft:"20px",
+    width:"72%",
+    display:"inline-block",
+    position:"relative",
+    bottom:"30px"
 
 }
 
