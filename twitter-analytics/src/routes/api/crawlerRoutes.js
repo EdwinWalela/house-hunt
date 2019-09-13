bed711c321d4c443d9a5988b954adb47ba1e0f54
edwinwalela <edwinwalela@gmail.com>
2019-09-13 17:17:17 +0300

    document code

--- twitter-analytics/src/routes/api/crawlerRoutes.js
+++ twitter-analytics/src/routes/api/crawlerRoutes.js
@@ -1,19 +1,26 @@
 const router = require("express").Router();
-const cheerio = require("cheerio")
+const cheerio = require("cheerio");
+const Tweet = require("../../models/Tweet");
 const fs = require("fs");
 
 const wordChecker = (word) =>{
     word = word.toLowerCase();
-    if(word.includes('tokens') || word.includes('meter')){
-        return false;
+    if(
+        word.includes('tokens') ||
+        word.includes('hakuna') ||
+        word.includes('m-pesa') ||
+        word.includes('meter')){
+            return false;
     }
     if(
         word.includes('outage') ||
         word.includes('loss') ||
         word.includes('potea') ||
         word.includes('no power') ||
-        word.includes('hakuna') ||
-        word.includes('outage')
+        word.includes('black out') ||
+        word.includes('blackout') ||
+        word.includes('darkness') ||
+        word.includes('outage') 
         ){
             return true;
     }else{
@@ -23,6 +30,7 @@ const wordChecker = (word) =>{
 
 router.get('/crawl',async(req,res)=>{
     let dom;
+    // read html from file
     try{
         dom = fs.readFileSync(__dirname+'/tweets.html','utf8');
     }catch(err){
@@ -32,20 +40,30 @@ router.get('/crawl',async(req,res)=>{
             err
         })
     }
+    // load html
     let $ = cheerio.load(dom);
+
     let results = [];
     $('li').map((i,el)=>{
         let body = $(el).find('p.TweetTextSize.js-tweet-text.tweet-text').text().trim();
         body = JSON.stringify(body);
         if(body.length>4){
+            // remove dirty tweets
             if(wordChecker(body)){
                 results.push(body);
             }
         }
     })
     
+    // Save crawled tweets to db
+    results.map(async tweet=>{
+        // await new Tweet({
+        //     body:tweet
+        // }).save();
+    })
+
     res.send({
-        msg:"OK",
+        msg:`${results.length} tweets saved to db`,
     })
 })
 
