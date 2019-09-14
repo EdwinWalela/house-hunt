2a54700cbc3c8d11ff77b08c39d5b256be8662df
edwinwalela <edwinwalela@gmail.com>
2019-09-14 13:46:31 +0300

    identify multi and single word areas

--- twitter-analytics/src/routes/api/tweetsRoutes.js
+++ twitter-analytics/src/routes/api/tweetsRoutes.js
@@ -16,32 +16,55 @@ router.get('/',async(req,res)=>{
     }
     res.send({
         msg:"OK",
+        count:results.length,
         results
     })
 });
 
 router.get('/analytics',async(req,res)=>{
+    let hits = [];
     // Get all Tweets
     let tweets = await Tweet.find({});
-    // Get one area
+    // Get one areas
     let areas = await Area.find({});
+
+    // Round 1: Identify muli-word areas
     // loop through all tweets
+    for(let i = 0; i < tweets.length;i++){
+        // remove punctuation marks
+        let tweet = tweets[i].body.replace(/[.,\/#@!?$%\^&\*;:{}=\-_`~()]/g,"");
+        // remove space left
+        tweet = tweet.replace(/\s{2,}/g," ");
+        // split tweet to words
+        let words = tweet.split(' ');
+        for(let j = 0; j < words.length-1; j++){
+            let wordPhrase = words[j].toLowerCase() +'-'+words[j+1].toLowerCase();
+            areas.map(area=>{
+                if(area.location == wordPhrase){
+                    hits.push(area.location);
+                }
+            })
+        }
+    }
+    // Round 2: Identify one-word areas
     tweets.map(tweet=>{
         // split each into array
         words = tweet.body.split(' ');
-        // compare each word with all areas
         words.map(word=>{
             areas.map(area=>{
                 word = word.toLowerCase();
                 area.location = area.location.toLowerCase();
                 if(area.location === word){
-                    // echo match
-                    console.log(area.location+" matched");
+                    hits.push(area.location);
                 }
             })
         })
     })
     
+    res.send({
+        msg:"OK",
+        hits
+    })
 })
 
 module.exports = router;
