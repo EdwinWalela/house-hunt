41a4e56671bc6c30b7363f625c8a7917c8a1f728
edwinwalela <edwinwalela@gmail.com>
2019-10-04 21:39:28 +0300

    steps layout

---
+++ client/src/Pages/Index/InteractiveSearch/Beds.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Beds(props) {
+    return (
+        <div style={containerStyle}>
+            
+        </div>
+    )
+}
+
+const containerStyle = {
+    border:"solid 1px",
+    padding:"20px",
+    margin:"20px auto",
+    width:"80%",
+}
+
+Location.propTypes = {
+
+}
+
+export default Beds
+
---
+++ client/src/Pages/Index/InteractiveSearch/Intrests.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Interests(props) {
+    return (
+        <div style={containerStyle}>
+            
+        </div>
+    )
+}
+
+const containerStyle = {
+    border:"solid 1px",
+    padding:"20px",
+    margin:"20px auto",
+    width:"80%",
+}
+
+Interests.propTypes = {
+
+}
+
+export default Interests
+
