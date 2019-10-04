663aad840812ca48e8b903d15ad9972140a02f78
edwinwalela <edwinwalela@gmail.com>
2019-10-04 19:46:44 +0300

    steps navigatorstyle

--- client/src/Pages/Index/StepNavigator.js
+++ client/src/Pages/Index/StepNavigator.js
@@ -2,13 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const iconStyle = (active) => {
+    active = active.split(",");
     let style = {
         fontSize:"1.5em",
         borderRadius:"5px",
         padding:"10px 20px",
         margin:"5px 5px"
     }
-    if(active){
+    if(active[0]=="true"){
         return {
             ...style,
             color:"#fff",
@@ -17,6 +18,7 @@ const iconStyle = (active) => {
     }else{
         return {
            ...style,
+           animation:"steps-bubble 1.5s ease-in-out infinite"
         }
     }
 }
@@ -24,10 +26,25 @@ const iconStyle = (active) => {
 function StepNavigator(props) {
     return (
         <div style={wrapperStyle}>
-            <i onClick={props.updateActiveStep.bind(this,0)} style={iconStyle(props.active == 0 ? true : false)} className="far fa-user"></i>
-            <i onClick={props.updateActiveStep.bind(this,1)} style={iconStyle(props.active == 1 ? true : false)} className="far fa-heart"></i>
-            <i onClick={props.updateActiveStep.bind(this,2)} style={iconStyle(props.active == 2 ? true : false)} className="fas fa-search"></i>
-            <i onClick={props.updateActiveStep.bind(this,3)} style={iconStyle(props.active == 3 ? true : false)} className="fas fa-sort-amount-down"></i>
+            <i 
+                onClick={props.updateActiveStep.bind(this,0)}
+                style={iconStyle(props.active == 0 ? "true,0" : "false")}
+                className="far fa-user">
+            </i>
+            <i 
+                onClick={props.updateActiveStep.bind(this,1)}
+                style={iconStyle(props.active == 1 ? "true,1" : "false")}
+                className="far fa-heart">
+            </i>
+            <i 
+                onClick={props.updateActiveStep.bind(this,2)}
+                style={iconStyle(props.active == 2 ? "true,2" : "false")}
+                className="fas fa-search"></i>
+            <i
+                onClick={props.updateActiveStep.bind(this,3)}
+                style={iconStyle(props.active == 3 ? "true,3" : "false")}
+                className="fas fa-sort-amount-down">
+            </i>
         </div>
     )
 }
