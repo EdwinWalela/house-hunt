73c9cdd3fdeedf9360d6048b0fbc76fc6a404fbb
edwinwalela <edwinwalela@gmail.com>
2019-10-05 13:02:37 +0300

    search animation

--- client/src/Pages/Index/InteractiveSearch/Location.js
+++ client/src/Pages/Index/InteractiveSearch/Location.js
@@ -8,19 +8,20 @@ function Location(props) {
     return (
         <div style={containerStyle}>
             <p style={titleStyle}>Choose a Location</p>
-            {/* <i style={iconStyle} className="fas fa-map-marker-alt"></i> */}
             <input className="search-input" style={inputStyle} type="text" placeholder="Start typing ..."/>
             <button style={buttonStyle}>Next</button>
+            <i style={iconStyle} className="fas fa-map-marker-alt location-icon"></i>
         </div>
     )
 }
 
 const containerStyle = {
-    background:"rgba(255,255,255,0.8)",
+    background:"rgba(255,255,255,1)",
     borderRadius:"5px",
     padding:"20px",
     margin:"20px auto",
     width:"90%",
+    maxWidth:"400px",
     height:"200px",
     textAlign:"center"
 }
@@ -28,21 +29,28 @@ const containerStyle = {
 const titleStyle = {
     fontWeight:"500",
     color:"#29335C",
+    letterSpacing:"1.2px",
 }
 
 const iconStyle = {
-    position:"absolute",
-    left:"22vw",
+    position:"relative",
+    display:"inline",
+    right:"36%",
+    bottom:"112px",
+    color:"#F3A712",
 }
 
 const inputStyle = {
     display:"block",
-    margin:"20px auto",
+    margin:"0px auto 20px auto",
     border:"none",
     borderBottom:"solid 2px #D3424E",
     background:"transparent",
-    padding:"5px",
+    padding:"5px 5px 5px 30px",
     width:"80%",
+    fontSize:"1.1em",
+    letterSpacing:"1.3px",
+    textTransform:"capitalize",
 }
 
 const buttonStyle = {
